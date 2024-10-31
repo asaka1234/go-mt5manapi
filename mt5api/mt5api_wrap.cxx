e85675fed979ba6c0ea6759d0dc43b622aa9b976
cyrildou <cyrildou@live.cn>
2024-10-31 11:45:40 +0800

    add print

--- mt5api/mt5api_wrap.cxx
+++ mt5api/mt5api_wrap.cxx
@@ -24822,8 +24822,9 @@ intgo _wrap_CMTManagerAPIFactory_Initialize__SWIG_0_mt5api_34c283227cc4d52d(CMTM
   arg2 = (LPCWSTR)*argp2;
 
 
-  //arg2 = "./MT5APIManager64.dll";
-  std::wcout<< "----start----" << arg2<<"\n";
+ LPCWSTR wstr = L"你好，世界！";
+
+  std::wcout<< "----start----" << wstr <<"\n";
 
 
   //printf("---start init---%s\n",arg2);
